1ae936bfcbc140fdf67691d8e0e5ce009e331534
penssake <penssake@gmail.com>
2020-04-01 17:59:13 -0700

    setup of basic scene

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Canvas />
+  </div>
+</template>
+
+<script>
+import Canvas from './components/Canvas.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Canvas
+  }
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/Canvas.vue
@@ -0,0 +1,35 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import * as THREE from "three";
+
+export default {
+  name: "Canvas",
+  props: {
+    msg: String
+  },
+  mounted() {    
+    const scene = new THREE.Scene();
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    // let mouseX = 0;
+    // let mouseY = 0;
+
+    const canvas = document.getElementById("canvas");
+    console.log(scene, renderer, canvas)
+  }
+};
+</script>
+<style lang="scss" scoped>
+.canvas {
+  background: limegreen;
+  width: 80vw;
+  height: 80vh;
+  margin: 5% 0 0 10%;
+  overflow: hidden;
+}
+</style>
