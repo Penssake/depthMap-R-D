03f42466d66277005b19f6e74be5957193df4d0f
penssake <penssake@gmail.com>
2020-04-03 11:59:44 -0700

    significant refactor for vue

--- src/components/MouseClickControls.vue
+++ src/components/MouseClickControls.vue
@@ -11,90 +11,90 @@ export default {
   name: "MouseClickControls",
   data() {
     return {
+      canvas: null,
+      scene: null,
+      camera: null,
       renderer: null,
-      canvas: null
+      controls: null,
+      lighting: null
     };
   },
   mounted() {
     this.canvas = document.getElementById("canvas");
-    const scene = new THREE.Scene();
-    // this camera is most similar to human eye / realism perspective
-    const camera = new THREE.PerspectiveCamera(
-      // field of view
-      75,
-      // aspect ration -- I want the canvas
-      window.innerWidth / 2 / (window.innerHeight / 1.43),
-      // near plane
-      1,
-      // far plane -- depends on how large model is -- then position zed and x || y position of object
-      500
-    );
 
-    camera.position.z = 10;
+    this.createScene();
+    this.createCamera();
     this.createRenderer();
-    // const renderer = new THREE.WebGLRenderer({ antialias: true });
-    // renderer.setClearColor("#fff");
-    // renderer.setSize(window.innerWidth / 2, window.innerHeight / 1.43);
-    // canvas.appendChild(renderer.domElement);
-
-    // this.renderer = renderer;
-
-    // enable orbit control
-    let controls = new OrbitControls(camera, this.renderer.domElement);
-    controls.enableDamping = true;
-    controls.campingFactor = 0.25;
-    controls.enableZoom = true;
-
-    // lighting && shadows
-    this.renderer.shadowMap.enabled = true;
-    this.renderer.shadowMap.type = THREE.PCFShadowMap;
-
-    var spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(0, 4000, 300);
-
-    spotLight.castShadow = true;
-
-    spotLight.shadow.mapSize.width = window.innerWidth / 2;
-    spotLight.shadow.mapSize.height = window.innerHeight / 1.43;
-    scene.add(spotLight);
-
-    // model
-    const loader = new OBJLoader();
-    let globalObj = null;
-    loader.setPath("./assets/models/");
-    loader.load("model.obj", function(object3D) {
-      globalObj = object3D;
-      globalObj.translateY(-2.5);
-      scene.add(globalObj);
-    });
-
-    // material
+    this.createControls();
+    this.createLighting();
 
     window.addEventListener("resize", () => {
       // to make renderer responsive
       this.renderer.setSize(window.innerWidth / 2, window.innerHeight / 1.43);
       // reset the cameras aspect ratio
-      camera.aspect = window.innerWidth / 2 / (window.innerHeight / 1.43);
+      this.camera.aspect = window.innerWidth / 2 / (window.innerHeight / 1.43);
       // needed for every update -- update matrix
-      camera.updateProjectionMatrix();
+      this.camera.updateProjectionMatrix();
     });
 
-    var animate = () => {
-      requestAnimationFrame(animate);
-      controls.update();
-      if (this.renderer) {
-        this.renderer.render(scene, camera);
-      }
-    };
-    animate();
+    this.animate();
   },
   methods: {
+    createScene() {
+      // this in reference to vues this, does not exist inside of your model loader
+      const scene = new THREE.Scene();
+      const loader = new OBJLoader();
+      loader.setPath("./assets/models/");
+      loader.load("model.obj", function(object3D) {
+        object3D.translateY(-2.5);
+        scene.add(object3D);
+      });
+      this.scene = scene;
+    },
+    createCamera() {
+      // (field of view(FOV), aspect ratio, near plane, far plane)
+      const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / 2 / (window.innerHeight / 1.43),
+        1,
+        500
+      );
+      camera.position.z = 10;
+      this.camera = camera;
+    },
     createRenderer() {
       const renderer = new THREE.WebGLRenderer({ antialias: true });
       renderer.setClearColor("#fff");
       renderer.setSize(window.innerWidth / 2, window.innerHeight / 1.43);
       this.renderer = renderer;
       this.canvas.appendChild(this.renderer.domElement);
+    },
+    createControls() {
+      // enable orbit control
+      let controls = new OrbitControls(this.camera, this.renderer.domElement);
+      controls.enableDamping = true;
+      controls.campingFactor = 0.25;
+      controls.enableZoom = true;
+      this.controls = controls;
+    },
+    createLighting() {
+      // lighting && shadows
+      this.renderer.shadowMap.enabled = true;
+      this.renderer.shadowMap.type = THREE.PCFShadowMap;
+
+      var spotLight = new THREE.SpotLight(0xffffff);
+      spotLight.position.set(0, 4000, 300);
+
+      spotLight.castShadow = true;
+
+      spotLight.shadow.mapSize.width = window.innerWidth / 2;
+      spotLight.shadow.mapSize.height = window.innerHeight / 1.43;
+      this.scene.add(spotLight);
+    },
+    animate() {
+      requestAnimationFrame(this.animate);
+      this.controls.update();
+      this.renderer.render(this.scene, this.camera);
     }
   }
 };
